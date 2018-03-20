6272b233d027ca07180d8d4793075ad30e1f9810
TCZdudu <1456164189@qq.com>
2018-03-20 13:29:53 +0800

    增加了扩展功能

--- WCProject/test1.c
+++
@@ -1,4 +0,0 @@
-﻿File write,name = new File(outputPath);
-            writename.createNe|wFile();
-            Buffe,redWriter out = new BufferedWrit,er(new FileWriter(writename));
-            out.close();
--- WCProject/wordtest.c
+++
@@ -1,4 +0,0 @@
-File write,name = new File(outputPath);
-            writename.createNe|wFile();
-            Buffe,redWriter out = new BufferedWrit,er(new FileWriter(writename));
-            out.close();
